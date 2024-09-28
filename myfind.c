d2e83383ed78908d255f990f057356dac36cd10c
muayad <moaiadmhana999@gmail.comm>
2024-09-28 10:57:23 +0200

    added SearchInPath and stringCompare functions

--- myfind.c
+++ myfind.c
@@ -3,6 +3,45 @@
 #include <stdlib.h>
 #include <stdbool.h>
 #include <sys/wait.h>
+#include <dirent.h>
+#include <string.h>
+
+// for insensitive compare between two strings
+int stringCompare(char *string1 , char* string2, int insensitive)
+{
+    if(insensitive == 1){return strcasecmp(string1,string2) == 0;}
+    else
+    {
+        return strcmp(string1,string2) == 0;
+    }
+}
+// normal search not recursive
+void searchFileInPath(char *searchPath, char *searchFile, int searchI)
+{
+    // The implementation of the searchFile function
+    DIR *dir;
+    struct dirent *file;
+    int pathFound = 0;
+    dir = opendir(searchPath);
+    if(dir == NULL)
+    {
+        printf("Error opening directory \n");
+    }
+    else
+    {
+        while((file = readdir(dir))!= NULL)
+        {
+            if(file->d_type == DT_REG && stringCompare(file->d_name,searchFile,searchI) == 1)
+            {
+                pathFound = 1;
+                break;
+            }
+        }
+        pathFound != 0 ? printf("File Found at %s/%s\n",searchPath,searchFile) : printf("File is not Found at this Path\n");    
+    }
+    closedir(dir);
+}
+
 
 int main(int argc, char *argv[])
 {
@@ -34,17 +73,17 @@ int main(int argc, char *argv[])
         printf("No file name or searchpath passed \n");
         exit(EXIT_FAILURE);
     }
-    // printf("Number of Argument passed %i\n",argc);
+    // Take the first argument as searchpath
     char *searchPath = argv[optind++];
     printf("The Searchpath passed is %s\n",searchPath);
-
+    // Files to Search
     if(optind < argc)
     {
         for(int c = optind ; c < argc; c++)
         {
             // The fork implementation
             printf("The Argument passed is %s\n",argv[c]);
+            searchFileInPath(searchPath,argv[c],searchI);
         }
     }
 }
-
